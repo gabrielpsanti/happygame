bdfb2ebbe1a56e08a207636f302579b7387dada9
Gabriel Santi <gabrielspessoti@gmail.com>
2026-02-19 01:25:13 -0300

    iniciando versionamento, primeiro commit

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import Nav from "@/components/Nav";
+import Inicio from '@/components/Inicio'
+
+export default function Home() {
+  return (
+    <div>
+
+      <Nav/>
+      <Inicio/>
+  
+      
+    </div>
+  );
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+    <footer className="bg-principal text-center mt-10 py-3 rounded-lg text-sm">
+        © 2025 HappyGame
+    </footer>
+    );
+}
